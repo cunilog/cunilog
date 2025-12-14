ad45cd35ba006a17c73f82c71969ea30e251d094
cunilog <cunilog@belmer.com>
2025-12-14 13:15:26 +0000

    Accumulated

--- src/c/combined/cunilog_combined.c
+++ src/c/combined/cunilog_combined.c
@@ -10632,26 +10632,6 @@ When		Who				What
 
 #endif
 
-/*
-	Note: These comments are only here for convenience. Refer to the
-	 ones in memstrstr.h. They might be more accurate.
-
-	Function:	memstrstr
-
-	The function returns a pointer to the first occurrence of s2 in s1.
-	If s2 cannot be found in s1 the return value is NULL. If size2 is
-	0 the function returns s1.
-
-	The function memstrstr is basically a replacement for strstr
-	(http://www.cppreference.com/stdstring/strstr.html) but does not require
-	NULL terminated strings. It can handle any buffer which can contain any
-	byte sequence including NUL characters.
-
-	The original version of this function has been taken from
-	http://www.koders.com/c/fid2330745E0E8C0A0F5E2CF94799642712318471D0.aspx?s=getopt#L459
-	which needed fixing first. The page has disappeared in the meantime (2018). The
-	Wayback Machine also holds no copy of it.
-*/
 char *memstrstr (const char *s1, size_t size1, const char *s2, size_t size2)
 {
 	size_t		count1		= size1;
@@ -10685,20 +10665,6 @@ char *memstrstr (const char *s1, size_t size1, const char *s2, size_t size2)
 	return NULL;
 }
 
-/*
-	Note: These comments are only here for convenience. Refer to the
-	 ones in memstrstr.h. They might be more accurate.
-
-	Like memstrstr but only returns the nth occurrance of s2 in s1 if
-	s2 has been found amount times in s1. Returns NULL if s1 does not
-	contain s2 amount times.
-
-	Example:
-	s1: "This is a test for memstrstrn."
-	s2: "e"
-	amount: 2 (second occurrance of s2 in s1)
-	Returns "emstrstrn."
-*/
 char *memstrstrn (char *s1, size_t size1, char *s2, size_t size2, size_t amount)
 {
 	char		*chF	= s1;
